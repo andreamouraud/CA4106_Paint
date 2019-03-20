6b0f326a02a4ceb5a06183389fa67b7a30aeb339
mourau_a <andrea.mouraud@epitech.eu>
2019-03-20 13:07:56 +0000

    renaming

--- Scripts/paint.js
+++ Scripts/paint.js
@@ -205,21 +205,19 @@ function changeObject(object) {
     currentObject.open(canvas);
 }
 
-function saveFile() {
-    console.log('a')
-  var lnk = document.createElement('a'), e;
-
-  /// the key here is to set the download attribute of the a tag
-  lnk.download = 'image.png';
-  lnk.href = canvas.toDataURL("image/png;base64");
-  if (document.createEvent) {
-    e = document.createEvent("MouseEvents");
-    e.initMouseEvent("click", true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
-    lnk.dispatchEvent(e);
-  } else if (lnk.fireEvent) {
-    lnk.fireEvent("onclick");
+function exportImage() {
+    var lnk = document.createElement('a'), e;
+  
+    lnk.download = 'image.png';
+    lnk.href = canvas.toDataURL("image/png;base64");
+    if (document.createEvent) {
+      e = document.createEvent("MouseEvents");
+      e.initMouseEvent("click", true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+      lnk.dispatchEvent(e);
+    } else if (lnk.fireEvent) {
+      lnk.fireEvent("onclick");
+    }
   }
-}
 
 function openFile(e) {
     var reader = new FileReader();
